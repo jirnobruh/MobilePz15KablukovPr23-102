1df78864176af30c8e711d085079744599bd2425
jirnobruh <jirnobruh@vk.com>
2026-03-22 23:24:18 +0700

    Created first page

--- app/src/main/java/com/example/pr15kablukovpr23_102/MainActivity.java
+++ app/src/main/java/com/example/pr15kablukovpr23_102/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pr15kablukovpr23_102;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Прячем системную панель для полного погружения
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        TextView tvSignUp = findViewById(R.id.tv_signup_link);
+
+        // Переход на экран логина
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
+
+        // "Свое действие" (на будущее): переход на экран регистрации
+        /*tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Пока можно просто выводить Toast или переходить на заглушку
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
